3ba89dc17ad75bd6f548eb5c83fe13076727a3cf
sub1120 <subhamsahu1120@gmail.com>
2023-04-26 22:32:44 +0530

    added about us page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,31 @@
-const About = () => {};
+const About = () => {
+  return (
+    <div className="bg-flag bg-cover space-y-10 px-32 py-10">
+      <div>
+        <h1 className="font-bold">Our Vision</h1>
+        <p>
+          MIC organisation vision is the economic development of poor and needy
+          people in its operational area through quality education, training and
+          capacity building. Eradicating poverty by providing a quality
+          accesories and environment to our unprivileged ones for creating their
+          strong foundation and bright future.
+        </p>
+      </div>
+      <div className="">
+        <h1 className="font-bold ">Our Mission</h1>
+        <p>
+          Our Aim is to create an advanced and enlightened Nation, mobilizing
+          the power of people against poverty. By developing the capacity and
+          skills of the members of socially and economically needful people in
+          India. One of the prime focus of MIC is to Empowering the
+          underprivileged and economically needful sections of society by Skill
+          Development & Education, eliminating Hunger, Women & Child
+          development, eliminating illiteracy, empowering and saving lives,
+          health awareness and safety, 24*7 emergency support.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default About;
