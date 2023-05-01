445aaefc30863d0018636859428f6816fc981403
sub1120 <subhamsahu1120@gmail.com>
2023-05-01 22:57:49 +0530

    fixed about us for chakraui

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,35 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
 const About = () => {
   return (
-    <div className="bg-flag bg-cover space-y-10 px-32 py-10">
-      <div>
-        <h1 className="font-bold">Our Vision</h1>
-        <p>
+    <Box>
+      <Box
+        as="section"
+        textAlign="center"
+        marginY={{ base: "20px", md: "30px" }}
+        marginX={{ base: "20px", md: "200px" }}
+      >
+        <Heading as="h1" size={["lg", "xl", "2xl"]}>
+          Our Vision
+        </Heading>
+        <Text>
           MIC organisation vision is the economic development of poor and needy
           people in its operational area through quality education, training and
           capacity building. Eradicating poverty by providing a quality
           accesories and environment to our unprivileged ones for creating their
           strong foundation and bright future.
-        </p>
-      </div>
-      <div className="">
-        <h1 className="font-bold ">Our Mission</h1>
-        <p>
+        </Text>
+      </Box>
+      <Box
+        as="section"
+        textAlign="center"
+        marginY={{ base: "20px", md: "30px" }}
+        marginX={{ base: "20px", md: "200px" }}
+      >
+        <Heading as="h1" size={["lg", "xl", "2xl"]}>
+          Our Mission
+        </Heading>
+        <Text>
           Our Aim is to create an advanced and enlightened Nation, mobilizing
           the power of people against poverty. By developing the capacity and
           skills of the members of socially and economically needful people in
@@ -22,9 +38,9 @@ const About = () => {
           Development & Education, eliminating Hunger, Women & Child
           development, eliminating illiteracy, empowering and saving lives,
           health awareness and safety, 24*7 emergency support.
-        </p>
-      </div>
-    </div>
+        </Text>
+      </Box>
+    </Box>
   );
 };
 
