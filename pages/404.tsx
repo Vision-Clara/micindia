2748955b7a28e8916f94a07ce3cab2e30bc55977
sub1120 <subhamsahu1120@gmail.com>
2024-06-03 23:44:34 +0530

    fix: 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,17 @@
-import { Image, Link } from "@chakra-ui/next-js";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import pageNotFound from "@/assets/images/404.png";
+import { ReactPropTypes } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const Custom404 = () => {
+const Custom404 = (props: ReactPropTypes) => {
   return (
     <Flex justifyContent="center" align="center" height="100vh" padding="20px">
       <Flex flexDirection="column" justifyContent="center" gap="30px">
         <Image src={pageNotFound} alt="404"></Image>
-        <Text textAlign="center">
+        <Text textAlign="center" textDecoration="underline">
           Go back to
-          <Link color="blue.500" textDecoration="underline" href="/">
+          <Link color="blue.500" href="/">
             Home Page
           </Link>
         </Text>
