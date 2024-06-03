6ec77db8f8a93310d3b1a8d721b5e7c61a54d9bf
sub1120 <subhamsahu1120@gmail.com>
2024-06-03 23:53:45 +0530

    fix: url

--- next.config.js
+++ next.config.js
@@ -11,7 +11,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "micindia.org" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.micindia.org",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
