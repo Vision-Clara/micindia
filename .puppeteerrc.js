da0c6e1af8b482f77b26ee79982f2b0ff0f30bab
sub1120 <subhamsahu1120@gmail.com>
2024-10-02 21:27:14 +0530

    fix: puppter cache issue

---
+++ .puppeteerrc.js
@@ -0,0 +1,9 @@
+import { join } from "node:path";
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+    // Changes the cache location for Puppeteer.
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
